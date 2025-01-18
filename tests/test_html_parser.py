5b86eb68e4e10e26184bd740c550aeaf583af40d
William <william.jewers@gmail.com>
2025-01-18 13:37:37 +0000

    Add tests/test_html_parser.py

---
+++ tests/test_html_parser.py
@@ -0,0 +1,11 @@
+import unittest
+from html_parser import parse_html
+
+class TestHTMLParser(unittest.TestCase):
+
+    def test_parse_html(self):
+        # Test parsing of HTML content
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
